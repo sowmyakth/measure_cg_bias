2ad1b0e8b78cb5bf093343732abd92bd5c39bb8c
sowmyakth <sowmya.spacebug@gmail.com>
2017-07-27 14:42:33 -0700

    changed method of calling seds

--- tests.py
+++ tests.py
@@ -11,19 +11,12 @@ def no_bulge():
     in_p = cg_fn.LSST_Args(bulge_frac=bfrac)
     filt = galsim.Bandpass('data/baseline/total_r.dat',
                            wave_type='nm').thin(rel_err=1e-4)
-    waves = np.linspace(10, 900, 2)
-    b_SED = galsim.SED("data/CWW_E_ext.sed", flux_type='flambda', 
-                       wave_type='Ang')
-    d_SED = galsim.SED("data/CWW_Im_ext.sed", flux_type='flambda', 
-                       wave_type='Ang')
-    c_SED = bfrac * b_SED + (1 - bfrac) * d_SED
-    in_p.b_SED, in_p.d_SED, in_p.c_SED = b_SED, d_SED, c_SED
+    in_p.b_SED, in_p.d_SED, in_p.c_SED = cg_fn.get_template_seds(in_p)
     gal = cg_fn.get_gal_cg(in_p)
     meas_args = cg_fn.meas_args()
     meas_args.bp = filt
     psf_args = cg_fn.psf_params()
     gcg, gnocg = cg_fn.calc_cg_crg(gal, meas_args, psf_args)
-    gtrue = meas_args.rt_g
     np.testing.assert_array_almost_equal((gcg / gnocg - 1).T[0], [0, 0],
                                          decimal=5)
 
@@ -34,20 +27,14 @@ def no_disk():
     in_p = cg_fn.LSST_Args(bulge_frac=bfrac)
     filt = galsim.Bandpass('data/baseline/total_r.dat',
                            wave_type='nm').thin(rel_err=1e-4)
-    waves = np.linspace(10, 900, 2)
-    b_SED = galsim.SED("data/CWW_E_ext.sed", flux_type='flambda', 
-                       wave_type='Ang')
-    d_SED = galsim.SED("data/CWW_Im_ext.sed", flux_type='flambda', 
-                       wave_type='Ang')
-    c_SED = bfrac * b_SED + (1 - bfrac) * d_SED
-    in_p.b_SED, in_p.d_SED, in_p.c_SED = b_SED, d_SED, c_SED
+    in_p.b_SED, in_p.d_SED, in_p.c_SED = cg_fn.get_template_seds(in_p)
     gal = cg_fn.get_gal_cg(in_p)
     meas_args = cg_fn.meas_args()
     meas_args.bp = filt
     psf_args = cg_fn.psf_params()
     gcg, gnocg = cg_fn.calc_cg_crg(gal, meas_args, psf_args)
-    gtrue = meas_args.rt_g
-    np.testing.assert_array_almost_equal((gcg/gnocg-1).T[0], [0,0], decimal=5)
+    np.testing.assert_array_almost_equal((gcg / gnocg - 1).T[0], [0, 0],
+                                         decimal=5)
 
 
 def same_bulge_disk():
@@ -55,43 +42,29 @@ def same_bulge_disk():
     in_p = cg_fn.LSST_Args(bulge_n=1, bulge_HLR=1.2)
     filt = galsim.Bandpass('data/baseline/total_r.dat',
                            wave_type='nm').thin(rel_err=1e-4)
-    waves = np.linspace(10, 900, 2)
-    b_SED = galsim.SED("data/CWW_E_ext.sed", flux_type='flambda', 
-                       wave_type='Ang')
-    d_SED = galsim.SED("data/CWW_Im_ext.sed", flux_type='flambda', 
-                       wave_type='Ang')
-    bfrac = in_p.bulge_frac
-    c_SED = bfrac * b_SED + (1 - bfrac) * d_SED
-    in_p.b_SED, in_p.d_SED, in_p.c_SED = b_SED, d_SED, c_SED
+    in_p.b_SED, in_p.d_SED, in_p.c_SED = cg_fn.get_template_seds(in_p)
     gal = cg_fn.get_gal_cg(in_p)
     meas_args = cg_fn.meas_args()
     meas_args.bp = filt
     psf_args = cg_fn.psf_params()
     gcg, gnocg = cg_fn.calc_cg_crg(gal, meas_args, psf_args)
-    gtrue = meas_args.rt_g
-    np.testing.assert_array_almost_equal((gcg/gnocg-1).T[0], [0,0], decimal=5)
+    np.testing.assert_array_almost_equal((gcg / gnocg - 1).T[0], [0, 0],
+                                         decimal=5)
 
 
 def same_sed():
     """bulge and disk have same sed, cg bias must be zero"""
-    in_p = cg_fn.LSST_Args()
+    in_p = cg_fn.LSST_Args(disk_SED_name='E')
     filt = galsim.Bandpass('data/baseline/total_r.dat',
                            wave_type='nm').thin(rel_err=1e-4)
-    waves = np.linspace(10, 900, 2)
-    b_SED = galsim.SED("data/CWW_E_ext.sed", flux_type='flambda', 
-                       wave_type='Ang')
-    d_SED = galsim.SED("data/CWW_Im_ext.sed", flux_type='flambda', 
-                       wave_type='Ang')
-    bfrac = in_p.bulge_frac
-    c_SED = bfrac * b_SED + (1 - bfrac) * d_SED
-    in_p.b_SED, in_p.d_SED, in_p.c_SED = b_SED, b_SED, b_SED
+    in_p.b_SED, in_p.d_SED, in_p.c_SED = cg_fn.get_template_seds(in_p)
     gal = cg_fn.get_gal_cg(in_p)
     meas_args = cg_fn.meas_args()
     meas_args.bp = filt
     psf_args = cg_fn.psf_params()
     gcg, gnocg = cg_fn.calc_cg_crg(gal, meas_args, psf_args)
-    gtrue = meas_args.rt_g
-    np.testing.assert_array_almost_equal((gcg/gnocg-1).T[0], [0,0], decimal=5)
+    np.testing.assert_array_almost_equal((gcg / gnocg - 1).T[0], [0, 0],
+                                         decimal=5)
 
 
 def with_cg():
@@ -99,20 +72,12 @@ def with_cg():
     in_p = cg_fn.LSST_Args()
     filt = galsim.Bandpass('data/baseline/total_r.dat',
                            wave_type='nm').thin(rel_err=1e-4)
-    waves = np.linspace(10, 900, 2)
-    b_SED = galsim.SED("data/CWW_E_ext.sed", flux_type='flambda',
-                       wave_type='Ang')
-    d_SED = galsim.SED("data/CWW_Im_ext.sed", flux_type='flambda',
-                       wave_type='Ang')
-    bfrac = in_p.bulge_frac
-    c_SED = bfrac * b_SED + (1 - bfrac) * d_SED
-    in_p.b_SED, in_p.d_SED, in_p.c_SED = b_SED, d_SED, c_SED
+    in_p.b_SED, in_p.d_SED, in_p.c_SED = cg_fn.get_template_seds(in_p)
     gal = cg_fn.get_gal_cg(in_p)
     meas_args = cg_fn.meas_args()
     meas_args.bp = filt
     psf_args = cg_fn.psf_params()
     gcg, gnocg = cg_fn.calc_cg_crg(gal, meas_args, psf_args)
-    gtrue = meas_args.rt_g
     np.testing.assert_raises(AssertionError,
                              np.testing.assert_array_almost_equal,
                              (gcg / gnocg - 1).T[0], [0, 0], decimal=5)
